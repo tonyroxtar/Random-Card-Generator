e04dee459348e6388fb31e66091cd76579e9e63e
Antonio Chamal <155495153+tonyroxtar@users.noreply.github.com>
2024-02-22 02:55:41 +0000

    hope is ok

--- src/app.js
+++ src/app.js
@@ -6,6 +6,43 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const randomSuit = generateRandomSuits();
+  document.querySelectorAll(".uppersuit, .bottomsuit").forEach(element => {
+    element.innerHTML = randomSuit;
+    if (randomSuit === "♦" || randomSuit === "♥") {
+      element.classList.add("red-symbol");
+    }
+  });
+
+  document.querySelector(".number").innerHTML = generateRandomNumbers();
 };
+
+let generateRandomNumbers = () => {
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+let generateRandomSuits = () => {
+  const suits = ["♦", "♥", "♠", "♣"];
+  let indexSuits = Math.floor(Math.random() * suits.length);
+  return suits[indexSuits];
+};
+
+console.log("Hello Rigo from the console!");
+
+//
